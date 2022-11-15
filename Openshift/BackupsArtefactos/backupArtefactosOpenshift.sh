5171aaa885c9fb25d39304166bfff0825a09584c
gtocino <guillermo.tocino@gmail.com>
2022-11-15 15:39:22 +0100

    Bucle para simplificar

--- Openshift/BackupsArtefactos/backupArtefactosOpenshift.sh
+++ Openshift/BackupsArtefactos/backupArtefactosOpenshift.sh
@@ -44,69 +44,11 @@ date=$(date '+%Y%m%d')
 mkdir $proyecto-$entorno-$date
 cd $proyecto-$entorno-$date
 
-#Crear los directorios si no existen
 echo "Se va a realizar un backup de DC, Services, Routes y Secrets"
 
-if [ ! -d "CMAPS" ] ;
+#Se recorre bucle sobre los objetos, se crea ditectorio si no existe y se guarda backup dentro de dicho directorio
+for objet in cm pods rc dc routes service secrets ; do
+    if [ ! -d "$objet" ] ;then mkdir $objet;    fi
+oc get $objet | awk '{print $1}' | grep -v NAME | xargs -t -I backup sh -c "oc get $objet/backup -o yaml > $objet/backup.yaml" 
 
-    then mkdir CMAPS
-fi
-
-if [ ! -d "PODS" ] ;
-
-    then mkdir PODS
-fi
-
-if [ ! -d "RCONTROLLERS" ] ;
-
-    then mkdir RCONTROLLERS
-fi
-
-if [ ! -d "DC" ] ;
-
-    then mkdir DC
-fi
-
-if [ ! -d "ROUTES" ] ;
-
-    then mkdir ROUTES
-fi
-
-if [ ! -d "SERVICE" ] ;
-
-    then mkdir SERVICE
-fi
-
-if [ ! -d "SECRETS" ] ;
-
-    then mkdir SECRETS
-fi
-
-#Obtener los arfectos correspondiente y almacenarlos en sus respectivos directorios
-cd ./RCONTROLLERS/
-oc get rc | awk '{print $1}' | grep -v NAME | xargs -t -I backup sh -c 'oc get rc/backup -o yaml > backup.yaml'
-cd ..
-
-cd ./CMAPS/
-oc get cm | awk '{print $1}' | grep -v NAME | xargs -t -I backup sh -c 'oc get cm/backup -o yaml > backup.yaml'
-cd ..
-
-cd ./PODS/
-oc get pods | awk '{print $1}' | grep -v NAME | xargs -t -I backup sh -c 'oc get pods/backup -o yaml > backup.yaml'
-cd ..
-
-cd ./DC/
-oc get dc | awk '{print $1}' | grep -v NAME | xargs -t -I backup sh -c 'oc get dc/backup -o yaml > backup.yaml'
-cd ..
-
-cd ./ROUTES/
-oc get routes | awk '{print $1}' | grep -v NAME | xargs -t -I backup sh -c 'oc get routes/backup -o yaml > backup.yaml'
-cd ..
-
-cd ./SERVICE/
-oc get service | awk '{print $1}' | grep -v NAME | xargs -t -I backup sh -c 'oc get service/backup -o yaml > backup.yaml'
-cd ..
-
-cd ./SECRETS/
-oc get secrets | awk '{print $1}' | grep -v NAME | xargs -t -I backup sh -c 'oc get secrets/backup -o yaml > backup.yaml'
-cd ..
+done
